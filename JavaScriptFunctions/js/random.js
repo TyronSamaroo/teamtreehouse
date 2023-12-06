367ed845344a781cc1d865ac1dd6f1116fb766df
Tyron <tyronsamaroo828@gmail.com>
2023-12-05 23:12:13 -0500

    Code for randomNumber function design using arrow function

--- JavaScriptFunctions/js/random.js
+++ JavaScriptFunctions/js/random.js
@@ -5,9 +5,13 @@
  * @param {number} upper - The highest number value.
  * @return {number} The random number value.
  */
+const randomNumber = (lower, upper ) => {
+    return Math.floor(Math.random() * (upper -  lower + 1)) + lower; //Math.random is 0 to ~~~.99
+}
 
-Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 
 // Call the function and pass it different values
-
+console.log(randomNumber(5, 20));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(100, 200));
 
