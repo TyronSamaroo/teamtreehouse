b85a26510caf3d85445886a4d032b5c0cd6ab3e0
Tyron <tyronsamaroo828@gmail.com>
2023-12-10 19:05:17 -0500

    Improve code with function to generate random color

--- JavaScriptLoops/RefactorChallenge/js/script.js
+++ JavaScriptLoops/RefactorChallenge/js/script.js
@@ -1,14 +1,24 @@
-let html = '';
-let red;
-let green;
-let blue;
-let randomRGB;
 
-for (let i = 1; i <= 10; i++) {
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
-html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+/**
+ * Generates a random RGB color.
+ * @returns {string} The random RGB color.
+ */
+function getRandomColor(){
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb( ${red}, ${green}, ${blue} )`;
 }
+
+let html = '';
+
+/**
+ * Loop from 1 to 10 to generate div elements with random background colors.
+ */
+for( let i = 1; i <= 10; i++ ){
+    const randomRGB = getRandomColor();
+    html += `<dibe style="background-color: ${randomRGB}">${i}</div>`;
+}
+
+// Set the innerHTML of the main element to the generated HTML string
 document.querySelector('main').innerHTML = html;
